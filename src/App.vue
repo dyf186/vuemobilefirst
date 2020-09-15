086b92b6fe06a41351e98caf926dd2242c63e38f
dengyf <18686883102@163.com>
2020-09-15 21:54:01 +0800

    设置动态rootValue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div>App1根组件</div>
+    <div class="box">666</div>
     <van-button type="default">默认按钮</van-button>
     <van-button type="primary">默认按钮</van-button>
     <van-button type="info">默认按钮</van-button>
@@ -11,9 +12,13 @@
 </template>
 <script>
 export default {
-  name: "app",
+  name: "app"
 };
 </script>
 
 <style lang="less">
+.box {
+  background-color: #aaa;
+  width: 375px;
+}
 </style>
