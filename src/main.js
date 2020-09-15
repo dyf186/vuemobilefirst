319441375aeb26b5e6f84ecf50765098d4bddc30
dengyf <18686883102@163.com>
2020-09-15 21:42:21 +0800

    rem适配基础配置

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Vant from 'vant'
-import 'vant/lib/index.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vant from "vant";
+import "vant/lib/index.css";
+import "amfe-flexible/index.min.js";
 
-Vue.use(Vant)
+Vue.use(Vant);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
