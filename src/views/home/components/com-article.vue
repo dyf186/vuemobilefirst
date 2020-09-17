6100f24121f07138d7ed4dae26fd21c542bce932
dengyf <18686883102@163.com>
2020-09-17 14:21:54 +0800

    文章组件独立

---
+++ src/views/home/components/com-article.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="scroll-wrapper">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item"></van-cell>
+    </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "com-article",
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {
+      list: [], // 接收数据
+      loading: false, // 加载动画
+      finished: false // 控制是否继续加载
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {
+    onLoad() {
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        this.loading = false;
+        if (this.list.length >= 40) {
+          this.finished = true; // 停止加载
+        }
+      }, 1000);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.scroll-wrapper {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,35 +5,28 @@
     > -->
     <van-tabs v-model="activeChannelIndex">
       <van-tab title="推荐">
-        <div class="scroll-wrapper">
-          <van-list
-            v-model="loading"
-            :finished="finished"
-            finished-text="没有更多了"
-            @load="onLoad"
-          >
-            <van-cell v-for="item in list" :key="item" :title="item"></van-cell>
-          </van-list>
-        </div>
+        <com-article></com-article>
       </van-tab>
-      <van-tab title="数据库">数据库内容展示</van-tab>
-      <van-tab title="后端">后端内容展示</van-tab>
+      <van-tab title="数据库">
+        <com-article></com-article>
+      </van-tab>
+      <van-tab title="后端">
+        <com-article></com-article>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
 
 <script>
+import ComArticle from "./components/com-article";
 export default {
   name: "home-index",
-  components: {},
+  components: { ComArticle },
   mixins: [],
   props: {},
   data() {
     return {
-      activeChannelIndex: 0,
-      list: [], // 接收数据
-      loading: false, // 加载动画
-      finished: false // 控制是否继续加载
+      activeChannelIndex: 0
     };
   },
   computed: {},
@@ -42,19 +35,7 @@ export default {
   mounted() {},
   beforeDestroy() {},
   destroyed() {},
-  methods: {
-    onLoad() {
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1);
-        }
-        this.loading = false;
-        if (this.list.length >= 40) {
-          this.finished = true; // 停止加载
-        }
-      }, 1000);
-    }
-  }
+  methods: {}
 };
 </script>
 
@@ -69,10 +50,6 @@ export default {
   }
   /deep/ .van-tab__pane {
     height: 100%;
-    .scroll-wrapper {
-      height: 100%;
-      overflow-y: auto;
-    }
   }
   /deep/ .van-tabs__line {
     background-color: #1989fa;
