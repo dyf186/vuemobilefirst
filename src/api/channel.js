09008e5f53fce45a5ce2a9c48df978ab294c1cfc
dengyf <18686883102@163.com>
2020-09-19 15:32:49 +0800

    api获取数据进行改造,存储到本地,第二次获取数据时走本地提高性能

--- src/api/channel.js
+++ src/api/channel.js
@@ -1,9 +1,26 @@
+import store from '@/store'
+const CHANNEL_KEY_TRAVEL = 'hm-channel-travel' // 游客key
+const CHANNEL_KEY_VIP = 'hm-channel-vip' // 登录用户key
 import request from "@/utils/request.js";
 export function apiChannelList() {
-    return request({
-        url: "/app/v1_0/user/channels",
-        method: "get"
-    });
+    return new Promise(async(resolve) => {
+            const key = store.state.user.token ? CHANNEL_KEY_VIP : CHANNEL_KEY_TRAVEL
+            const localChannels = localStorage.getItem(key)
+            if (localChannels) {
+                resolve({ channels: JSON.parse(localChannels) })
+            } else {
+                const result = await request({
+                    url: "/app/v1_0/user/channels",
+                    method: "get"
+                });
+                localStorage.setItem(key, JSON.stringify(result.channels))
+                resolve(result)
+            }
+        })
+        // return request({
+        //     url: "/app/v1_0/user/channels",
+        //     method: "get"
+        // });
 }
 export function apiChannelAll() {
     return request({
