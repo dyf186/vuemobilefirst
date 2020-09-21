6eb59beef3af12a0854372a284e0a1446507aa73
dengyf <18686883102@163.com>
2020-09-21 16:30:26 +0800

    绘制详情页面结构

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,9 +6,38 @@
       @click-left="$router.back()"
       title="文章详情"
     ></van-nav-bar>
-    <van-button round size="small" @click="$store.commit('clearToken')"
-      >清除token</van-button
-    >
+    <div class="detail">
+      <h3 class="title">美女</h3>
+      <div class="author">
+        <van-image
+          round
+          width="1rem"
+          height="1rem"
+          fit="fill"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        ></van-image>
+        <div class="text">
+          <p class="name">123</p>
+          <p class="time">456</p>
+        </div>
+        <van-button round size="small" type="info">+ 关注</van-button>
+      </div>
+      <div class="content">
+        <p>内容内容内容内容内容内容</p>
+      </div>
+      <div class="zan">
+        <van-button
+          round
+          size="small"
+          class="active"
+          plain
+          icon="like-o"
+          style="margin-right:8px;"
+          >点赞</van-button
+        >
+        <van-button round size="small" plain icon="delete">不喜欢</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,4 +60,60 @@ export default {
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.container {
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.detail {
+  padding: 0 20px 88px;
+  margin-top: 92px;
+  .title {
+    font-size: 36px;
+    line-height: 2;
+  }
+  .zan {
+    text-align: center;
+    padding: 20px 0;
+    .active {
+      border-color: red;
+      color: red;
+    }
+  }
+  .author {
+    padding: 20px 0;
+    display: flex;
+    position: sticky;
+    background-color: #fff;
+    top: 92px;
+    z-index: 2;
+    .text {
+      flex: 1;
+      padding-left: 20px;
+      line-height: 1.5;
+      .name {
+        font-size: 14px;
+        margin: 0;
+      }
+      .time {
+        margin: 0;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+  .content {
+    padding: 40px 0;
+    overflow: hidden;
+    white-space: pre-wrap;
+    word-break: break-all;
+    p {
+      font-size: 28px;
+    }
+    /deep/ code {
+      white-space: pre-wrap;
+    }
+  }
+}
+</style>
