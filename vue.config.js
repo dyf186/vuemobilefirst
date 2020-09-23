2a9abb3c1cfc1ae78fec54cd3a8c66e99fe9e0c4
dengyf <18686883102@163.com>
2020-09-23 19:01:42 +0800

    打包配置-修改静态资源文件访问路径

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
     lintOnSave: false,
     devServer: {
         open: true,
-        port: '12233'
-    }
-}
+        port: "12233"
+    },
+    // 打包时如下配置
+    // 配置dist/index.html文件引用静态文件的根地址信息为当前目录
+    publicPath: "./"
+};
