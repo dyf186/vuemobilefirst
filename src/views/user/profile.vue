9b57c964e87ddd656a9932c764c177e8ce0d1e6a
dengyf <18686883102@163.com>
2020-09-23 09:40:57 +0800

    编辑资料的页面布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -7,6 +7,21 @@
       right-text="保存"
       @click-right="save()"
     ></van-nav-bar>
+    <van-cell-group>
+      <van-cell is-link title="头像" center>
+        <van-image
+          round
+          slot="default"
+          width="56"
+          height="56"
+          fit="cover"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        ></van-image>
+      </van-cell>
+      <van-cell is-link title="名称" value="用户名称"></van-cell>
+      <van-cell is-link title="性别" value="男"></van-cell>
+      <van-cell is-link title="生日" value="2020-1-1"></van-cell>
+    </van-cell-group>
   </div>
 </template>
 
